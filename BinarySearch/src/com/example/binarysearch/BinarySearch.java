9020f6c0f39a75b8ba8d890c848a50106b4f843d
Eddie Rangel <erangeljr@gmail.com>
2012-09-21 21:37:24 -0700

    Created BinarySearch.java
    Createad a class file.
    Created a search method that takes an int for a parameter.

---
+++ BinarySearch/src/com/example/binarysearch/BinarySearch.java
@@ -0,0 +1,17 @@
+package com.example.binarysearch;
+
+import android.app.Activity;
+
+public class BinarySearch extends Activity{
+	
+	private Boolean found;
+	
+	public void search(int number)
+	{
+		if(!found)
+		{
+			
+		}
+	}
+
+}
